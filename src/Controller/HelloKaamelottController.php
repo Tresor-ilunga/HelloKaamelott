50b14813810f857672eba036b3b7eaa3c78f7a80
Tresor-ilunga <19im065@esisalama.org>
2023-06-18 21:24:17 +0200

    Add Docker environment

--- src/Controller/HelloKaamelottController.php
+++ src/Controller/HelloKaamelottController.php
@@ -1,14 +1,33 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\DecodingExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\ServerExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
 
+/**
+ * Class HelloKaamelottController
+ *
+ * @author tresor-ilunga <ilungat82@gmail.com>
+ */
 class HelloKaamelottController extends AbstractController
 {
+    /**
+     * @throws TransportExceptionInterface
+     * @throws ServerExceptionInterface
+     * @throws RedirectionExceptionInterface
+     * @throws DecodingExceptionInterface
+     * @throws ClientExceptionInterface
+     */
     #[Route('/', name: 'app_hello_kaamelott')]
     public function index(HttpClientInterface $client): Response
     {
